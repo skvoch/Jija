8ccde2cd74bc0ba323b110ce1d6f3830139be5ee
skvoch <skvoch@gmail.com>
2019-09-21 22:39:25 +0300

    Added: InputComponent

---
+++ src/core/ecs/Components/InputComponent.h
@@ -0,0 +1,12 @@
+#ifndef INPUTCOMPONENT_H
+#define INPUTCOMPONENT_H
+
+
+#include "../IComponent.h"
+class InputComponent : public IComponent
+{
+public:
+    int key = 0;
+};
+
+#endif // INPUTCOMPONENT_H
